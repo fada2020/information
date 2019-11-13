faa9467bc1df5dc7c986e9412f9f2ddf7d68fd88
sunjm <sunjm@ais-info.co.jp>
2019-11-13 17:23:16 +0900

    db change

---
+++ src/main/java/jp/co/info/ais/asm/domain/CodeDetail.java
@@ -0,0 +1,16 @@
+package jp.co.info.ais.asm.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+public class CodeDetail {
+	String codeMasterId;
+	String codeDetailId;
+	String codeDetailName;
+	String item1;
+	String item2;
+
+}
