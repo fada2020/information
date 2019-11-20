36b25670b673fc779cf07d940323441e6bdb0006
yoi1215 <yangdj@ais-info.co.jp>
2019-11-20 12:05:13 +0900

    資産貸与グラフについてController修訂

--- src/main/java/jp/co/info/ais/asm/controller/MainController.java
+++ src/main/java/jp/co/info/ais/asm/controller/MainController.java
@@ -72,11 +72,11 @@ public class MainController {
 			model.addAttribute("hardPercent", hardPercent);
 			model.addAttribute("softPercent", softPercent);
 
-			//  貸与現況グラフ
+			// 貸与現況グラフ
 			ArrayList<Dashboard> rslist = new ArrayList<Dashboard>();
 			rslist = dashboardService.rslist();
 			ArrayList<String> rentItem = new ArrayList<String>();
-			// rentItem
+			// 【目録】貸与現況グラフ
 			for (int i = 0; i < rslist.size(); i++) {
 				if (i + 1 < rslist.size()) {
 					if (!rslist.get(i).getKubunName().equals(rslist.get(i + 1).getKubunName())) {
@@ -89,15 +89,19 @@ public class MainController {
 				}
 			}
 			model.addAttribute("rentItem", rentItem);
-
-
+			// 【数量】貸与現況グラフ：保有、貸与
+			model.addAttribute("DT001", 0);	model.addAttribute("DT002", 0);
+			model.addAttribute("NB001", 0); model.addAttribute("NB002", 0);
+			model.addAttribute("TBL001", 0); model.addAttribute("TBL002", 0);
+			model.addAttribute("MD001", 0); model.addAttribute("MD002", 0);
+			model.addAttribute("DP001", 0); model.addAttribute("DP002", 0);
+			model.addAttribute("KBD001", 0); model.addAttribute("KBD002", 0);
+			model.addAttribute("MS001", 0); model.addAttribute("MS002", 0);
 
 			for (Dashboard item : rslist) {
 				if (item.getKubunCode().equals("002")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("DT001", item.getTypeCnt());
-					} else {
-						model.addAttribute("DT001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("DT002", item.getTypeCnt());
@@ -115,8 +119,6 @@ public class MainController {
 				if (item.getKubunCode().equals("003")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("NB001", item.getTypeCnt());
-					} else {
-						model.addAttribute("NB001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("NB002", item.getTypeCnt());
@@ -133,8 +135,6 @@ public class MainController {
 				if (item.getKubunCode().equals("004")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("TBL001", item.getTypeCnt());
-					} else {
-						model.addAttribute("TBL001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("TBL002", item.getTypeCnt());
@@ -150,8 +150,6 @@ public class MainController {
 				if (item.getKubunCode().equals("005")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("MD001", item.getTypeCnt());
-					} else {
-						model.addAttribute("MD001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("MD002", item.getTypeCnt());
@@ -166,8 +164,6 @@ public class MainController {
 				if (item.getKubunCode().equals("006")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("DP001", item.getTypeCnt());
-					} else {
-						model.addAttribute("DP001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("DP002", item.getTypeCnt());
@@ -181,8 +177,6 @@ public class MainController {
 				if (item.getKubunCode().equals("007")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("KBD001", item.getTypeCnt());
-					} else {
-						model.addAttribute("KBD001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("KBD002", item.getTypeCnt());
@@ -195,8 +189,6 @@ public class MainController {
 				if (item.getKubunCode().equals("008")) {
 					if(item.getStatusCode().equals("001")) {
 						model.addAttribute("MS001", item.getTypeCnt());
-					} else {
-						model.addAttribute("MS001", 0);
 					}
 					if(item.getStatusCode().equals("002")) {
 						model.addAttribute("MS002", item.getTypeCnt());
