edff74ac14c5486d6b350bb4f5bc87677cd195a0
AIS191101 <AIS191101@DESKTOP-GF5IIBF>
2019-11-28 15:16:26 +0900

    codeDetail

---
+++ src/main/java/jp/co/info/ais/ams/mapper/CodeDetailMapper.java
@@ -0,0 +1,39 @@
+package jp.co.info.ais.ams.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import jp.co.info.ais.ams.domain.CodeDetail;
+
+/**
+ *
+ * @author AIS191101
+ *
+ */
+@Mapper
+public interface CodeDetailMapper {
+
+
+	List<CodeDetail> selectCodeDetailList(CodeDetail condition);
+
+	int selectCount(CodeDetail condition);
+
+	int CodeDetailListCheck(CodeDetail masterCode);
+
+	void CodeDetailInsert(CodeDetail codeMasterId);
+
+	int updateCodeDetail(CodeDetail codeMasterId);
+
+	int deleteCodeDetail(String codeMasterId);
+
+	List<CodeDetail> selectProductCode();
+
+	List<CodeDetail> selectStateCode();
+
+	List<CodeDetail> selectKubunCode();
+
+	List<CodeDetail> selectMasterCodeId();
+
+
+}
